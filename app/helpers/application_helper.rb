61ac04c83edf0123f516f2d575c30906f1143cd7
Levan Mdivani <leomdiv@gmail.com>
2014-02-06 10:28:12 +0000

    heroku resize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "FAKE MUSIC LTD"
+    base_title = "BBK LIMITED"
     if page_title.empty?
       base_title
     else
