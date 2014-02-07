65188ec7f096fa4f0006d3a2b5c7cf159c9597ea
Levan Mdivani <leomdiv@gmail.com>
2014-02-07 14:28:06 +0000

    Finish user edit, update, index and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,71 @@
 class UsersController < ApplicationController
-before_action :signed_in_user, only: [:edit, :update]
-before_action :correct_user,   only: [:edit, :update]
-   def show
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def show
     @user = User.find(params[:id])
-   end
-   def index
-     @users = User.all
-     end
-   def new
-     @user = User.new
-   end
+  end
+  
+  def new
+    @user = User.new
+  end
+  def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    end
+    def admin_user
+          redirect_to(root_url) unless current_user.admin?
+        end
    
-   def create
-       @user = User.new(user_params)
-       if @user.save
-         flash[:success] = "Welcome to BBK LIMITED!"
-         redirect_to @user
-       else
-         render 'new'
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to BBK LIMITED!"
+      redirect_to @user
+    else
+      render 'new'
     end
-    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
     
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end     
+  end
+    
+  def index
+    @title = "All users"
+    @users = User.paginate(page: params[:page])
+  end
 
-    def edit
-       @user = User.find(params[:id])
-    end
-    def update
-      @user = User.find(params[:id])
-      if @user.update_attributes(user_params)
-        flash[:success] = "Profile updated"
-        redirect_to @user
-      else
-        render 'edit'
-    end
-    end
+  private
 
-     private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
+  end
+  # Before filters
 
-    def user_params
-         params.require(:user).permit(:name, :email, :password,
-                                      :password_confirmation)
-    end
-    # Before filters
-
-    def signed_in_user
+  def signed_in_user
     unless signed_in?
       store_location     
       redirect_to signin_url, notice: "Please sign in." 
     end
   end
-    def correct_user
-          @user = User.find(params[:id])
-          redirect_to(root_url) unless current_user?(@user)
-    end
+    
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
    
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,15 +16,27 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin
+     def describe
+     end
+  describe "with admin attribute set to 'true'" do
+   before do
+          @user.save!
+          @user.toggle!(:admin)
+  end
+
+        it { should be_admin }
+  end}
   
   describe "remember token" do
       before { @user.save }
       its(:remember_token) { should_not be_blank }
     end
   
-  
+    
   
   it "should be valid" do
     expect(@user).to be_valid
